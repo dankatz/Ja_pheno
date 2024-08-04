20dff279638c1cd5d38de384980535aa01f597c8
dankatz <dankatz512@gmail.com>
2024-08-03 22:41:15 -0400

    adding in comparison for 2019-2020 2020-2021 NAB vs pheno predictions

--- ripening_vs_airborne_p.R
+++ ripening_vs_airborne_p.R
@@ -5,13 +5,196 @@ library(readr)
 library(dplyr)
 library(tidyr)
 library(imputeTS)
-library(ggpmisc)
+#library(ggpmisc)
 library(lubridate)
 library(sf)
 
+### compare NAB data during 2019-2020 and 2020-2021 with pollen model #######################################
+nab <- readr::read_csv("C:/Users/danka/Box/texas/NAB/NAB2009_2021_tx_epi_pollen_220831.csv") %>%
+  mutate(Cupressaceae_m = cup_all_m) %>% 
+   filter(NAB_station != "Waco B") %>%
+   filter(NAB_station != "College Station") %>%
+   filter(NAB_station != "Dallas") %>%
+  # filter(NAB_station != "Flower Mound") %>%
+  filter(mo < 4 | mo > 11) %>%
+  mutate(day_exp = case_when(mo < 4 ~ yday(date) + 21,
+                             mo > 10 ~ yday(date) -  yday(dmy(paste(10, 12, year, sep = "/")))),
+         yr_exp = case_when(mo < 4 ~ paste0("yr_", (year(date) -1 ), "_", year(date)),
+                            mo > 10 ~ paste0("yr_", year(date), "_", (year(date) + 1)))) %>% 
+  filter(yr_exp == "yr_2019_2020" | yr_exp == "yr_2020_2021")
+  #filter(year > 2018 ) %>% 
+
+#map of Texas with NAB data
+tx_boundary <- read_sf("C:/Users/danka/Box/texas/statewide_abundance/Texas_State_Boundary/Texas_State_Boundary.shp")
+NAB_subset <- nab %>% filter(date == "2019-01-08")
+ggplot(tx_boundary) +   geom_sf(data = tx_boundary, colour = "black", fill = NA) +
+  geom_point(aes(x = Long, y = Lat, color = Cupressaceae), #size = Observation_Date,#col = hilo2), pollen / max_p
+             data = NAB_subset, alpha = .95, size = 5)  + #scale_color_continuous(low = "blue", high = "red", name = "relative rank") + 
+  xlab("") + ylab("") + #theme_few() + 
+  scale_color_viridis_c(name = "airborne pollen (grains/m3)", option = "viridis") +
+  theme(panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank(),
+        panel.background = element_blank()) +
+  coord_sf(datum=NA) #removes sf induced gridlines
+
+
+#filter out low data years
+nab_low_na <- nab #%>% filter(n_NA < 40)
+
+
+#calculate cumulative pollen and relative position in accumulated pollen season
+cup_m_season_total <- nab_low_na %>%
+  group_by(NAB_station, yr_exp) %>%
+  summarize(cup_m_total = sum(Cupressaceae_m, na.rm = TRUE))
+
+nab_low_na <- left_join(nab_low_na, cup_m_season_total)  
+
+nab_low_na <- nab_low_na %>%
+  arrange(NAB_station, yr_exp, day_exp) %>%
+  filter(!is.na(Cupressaceae_m)) %>% 
+  group_by(NAB_station, yr_exp) %>%
+  mutate(cup_m_cumu = cumsum(Cupressaceae_m),
+         cup_m_cumu_dist = cup_m_cumu/cup_m_total)
+
+#median and 50% and 95% pollen season
+nearest_25 <- nab_low_na %>% group_by(NAB_station, yr_exp) %>% 
+  filter(abs(cup_m_cumu_dist - 0.25) == min(abs(cup_m_cumu_dist - 0.25)))
+
+nearest_50 <- nab_low_na %>% group_by(NAB_station, yr_exp) %>% 
+  filter(abs(cup_m_cumu_dist - 0.5) == min(abs(cup_m_cumu_dist - 0.5)))
+
+nearest_75 <- nab_low_na %>% group_by(NAB_station, yr_exp) %>% 
+  filter(abs(cup_m_cumu_dist - 0.75) == min(abs(cup_m_cumu_dist - 0.75))) %>%
+  slice(1)
+
+
+#some data visualization
+ggplot(nab_low_na, aes(x = day_exp, y = Cupressaceae_m + 1, color = cup_m_cumu_dist)) + geom_line() + theme_bw() + facet_grid(yr_exp~NAB_station) +
+  scale_y_log10() + 
+  scale_color_viridis_c() +
+  geom_point(data = nearest_25, aes(x= day_exp, y = Cupressaceae_m + 1 ), col = "red") +
+  geom_point(data = nearest_50, aes(x= day_exp, y = Cupressaceae_m + 1 ), col = "red") +
+  geom_point(data = nearest_75, aes(x= day_exp, y = Cupressaceae_m + 1 ), col = "red") 
+
+ggplot(nab_low_na, aes(x = day_exp, y = cup_m_cumu_dist + 1)) + geom_line() + theme_bw() + facet_grid(yr_exp~NAB_station) +
+  scale_y_log10() + 
+  scale_color_viridis_c() +
+  geom_point(data = nearest_25, aes(x= day_exp, y = cup_m_cumu_dist + 1 ), col = "red") +
+  geom_point(data = nearest_50, aes(x= day_exp, y = cup_m_cumu_dist + 1 ), col = "red") +
+  geom_point(data = nearest_75, aes(x= day_exp, y = cup_m_cumu_dist + 1 ), col = "red") 
+
+
+#compare to raster surfaces of predictions
+
+pheno_preds_NAB <- read_csv("C:/Users/danka/Box/texas/pheno/manual_obs_models/NAB_opening_preds_aprilssm_25km.csv") %>%
+  rename(day_exp = day_experiment,
+         yr_exp = fs_year) %>%
+  dplyr::select(-ID, -lat, -long) %>%
+  filter(!is.na(rel_opening)) %>%
+  mutate(NAB_station = case_when(NAB_station == "Georgetown" ~ "Austin",
+                                 NAB_station != "Georgetown"~ NAB_station)) %>% 
+  filter(NAB_station == "Austin" | NAB_station == "San Antonio A" | NAB_station == "San Antonio B"|
+           NAB_station == "Waco A" | NAB_station == "Waco B" | NAB_station == "College Station" |
+           NAB_station == "Flower Mound" | NAB_station == "Dallas" | NAB_station == "Houston")
+
+pheno_season_total <- pheno_preds_NAB %>%
+  group_by(NAB_station, yr_exp) %>%
+  summarize(opening_total = sum(rel_opening))
+
+pheno_preds_NAB <- left_join(pheno_preds_NAB, pheno_season_total)  
+
+pheno_preds_NAB <- pheno_preds_NAB %>%
+  arrange(NAB_station, yr_exp, day_exp) %>%
+  group_by(NAB_station, yr_exp) %>%
+  mutate(opening_cumu = cumsum(rel_opening),
+         opening_dist = opening_cumu/opening_total)
+
+#25 and 50% and 75% pollen season
+nearest_25_pheno <- pheno_preds_NAB %>% group_by(NAB_station, yr_exp) %>% 
+  filter(abs(opening_dist - 0.25) == min(abs(opening_dist - 0.25)))%>%
+  rename(day_exp_p = day_exp)
+
+nearest_50_pheno <- pheno_preds_NAB %>% group_by(NAB_station, yr_exp) %>% 
+  filter(abs(opening_dist - 0.5) == min(abs(opening_dist - 0.5))) %>%
+  rename(day_exp_p = day_exp)
+
+nearest_75_pheno <- pheno_preds_NAB %>% group_by(NAB_station, yr_exp) %>% 
+  filter(abs(opening_dist - 0.75) == min(abs(opening_dist - 0.75))) %>%
+  slice(1)%>%
+  rename(day_exp_p = day_exp)
+
+
+pheno_preds_NAB 
+nab_low_na
+
+nab_pheno_c <- left_join(nab_low_na, pheno_preds_NAB)
+
+#visualize phenology model at NAB stations
+nab_pheno_c %>%
+  ggplot(aes(x = day_exp, y = rel_opening)) + geom_line() + facet_grid(NAB_station ~ yr_exp) + theme_bw()
+
+
+#compare time series to NAB
+nab_pheno_c %>% #filter(env_year > 2008) %>%
+  
+  ggplot(aes(x = day_exp, y = Cupressaceae_m + 1, color = opening_dist)) + geom_line() + theme_bw() + 
+  facet_grid(NAB_station ~ year, scales = "free_y") +
+  #scale_y_log10() + 
+  scale_color_viridis_c() +
+  geom_point(data = nearest_25, aes(x= day_exp, y = Cupressaceae_m + 1 ), col = "red") +
+  geom_point(data = nearest_50, aes(x= day_exp, y = Cupressaceae_m + 1 ), col = "red") +
+  geom_point(data = nearest_75, aes(x= day_exp, y = Cupressaceae_m + 1 ), col = "red") +
+  geom_point(data = nearest_25_pheno, aes(x= day_exp_p, y = 1 ), col = "blue") +
+  geom_point(data = nearest_50_pheno, aes(x= day_exp_p, y = 1 ), col = "blue") +
+  geom_point(data = nearest_75_pheno, aes(x= day_exp_p, y = 1 ), col = "blue") 
+
+#put both time series on the same fig
+nab_pheno_c %>%
+  ggplot(aes(x = day_exp, y = rel_opening)) + geom_line() + 
+  facet_grid(NAB_station ~ yr_exp, scales = "free_y") + ggthemes::theme_few() +
+  geom_point(data = nab_pheno_c, aes(x = day_exp, y = Cupressaceae/50000), alpha = 0.1, col = "red") + 
+  geom_line(data = nab_pheno_c, aes(x = day_exp, y = Cupressaceae/50000), alpha = 0.5, col = "red") 
+
+#focus in on just a station in a year
+NAB_station_focal <- "Austin"
+focal_year <- "yr_2020_2021"
+ts_panel_pheno_pred <- nab_pheno_c %>%
+  filter(NAB_station == NAB_station_focal) %>% 
+  filter(yr_exp == focal_year) %>% 
+  ggplot(aes(x = date, y = rel_opening * 100)) + geom_line() + 
+  xlab("date")+ ylab("cone opening (% within 25 km)") + ggthemes::theme_few() +
+  theme(text = element_text(size = 20))
+
+ts_panel_NAB <- nab_pheno_c %>%
+  filter(NAB_station == NAB_station_focal) %>% 
+  filter(yr_exp == focal_year) %>% 
+  ggplot(aes(x = date, y = Cupressaceae)) + geom_line() + geom_point()+
+  xlab("date")+ ylab(airborne~pollen~(grains~"/"~m^3)) + ggthemes::theme_few() +
+  theme(text = element_text(size = 20))
+
+cowplot::plot_grid(ts_panel_pheno_pred, ts_panel_NAB, ncol = 1)
+ggsave()  
+
+
+
+
+#compare season mid-points directly
+nearest_50_nab_pheno <- left_join(nearest_50, nearest_50_pheno)
+
+nearest_50_nab_pheno %>%
+  #filter(yr_exp != "yr_2015_2016") %>%
+  ggplot(aes(x = day_exp_p, y = day_exp, color = NAB_station)) + geom_point() + theme_bw() + 
+  geom_abline(slope = 1, intercept = 0, lty =2) +
+  geom_smooth(method = "lm", se = FALSE)
+
+summary(lm(day_exp ~ day_exp_p, data = nearest_50_nab_pheno))
+
+
+
+
 ### NAB data in Texas ############################################################
 # load in NAB data 
-nab <- readr::read_csv("C:/Users/dsk856/Box/texas/NAB/NAB_pollen_modeled_linear_interp_201002.csv") %>%
+nab <- readr::read_csv("C:/Users/danka/Box/texas/NAB/NAB_pollen_modeled_linear_interp_201002.csv") %>%
   # filter(NAB_station != "Houston") %>%
   # filter(NAB_station != "College Station") %>%
   # filter(NAB_station != "Dallas") %>%
@@ -24,7 +207,7 @@ nab <- readr::read_csv("C:/Users/dsk856/Box/texas/NAB/NAB_pollen_modeled_linear_
            
 
 #map of Texas with NAB data
-tx_boundary <- read_sf("C:/Users/dsk856/Box/texas/statewide_abundance/Texas_State_Boundary/Texas_State_Boundary.shp")
+tx_boundary <- read_sf("C:/Users/danka/Box/texas/statewide_abundance/Texas_State_Boundary/Texas_State_Boundary.shp")
 NAB_subset <- nab %>% filter(date == "2019-01-08")
 ggplot(tx_boundary) +   geom_sf(data = tx_boundary, colour = "black", fill = NA) +
   geom_point(aes(x = Long, y = Lat, color = Cupressaceae), #size = Observation_Date,#col = hilo2), pollen / max_p
@@ -103,7 +286,7 @@ ggplot(nab_low_na, aes(x = day_exp, y = cup_m_cumu_dist + 1)) + geom_line() + th
 
 
 ### cone opening preds compared to NAB #########################################################################
-pheno_preds_NAB <- read_csv("C:/Users/dsk856/Box/texas/pheno/manual_obs_models/NAB_opening_preds_jan_dec_prcp_25km.csv") %>%
+pheno_preds_NAB <- read_csv("C:/Users/danka/Box/texas/pheno/manual_obs_models/NAB_opening_preds_jan_dec_prcp_25km.csv") %>%
                     rename(day_exp = day_experiment,
                            yr_exp = fs_year) %>%
                     dplyr::select(-ID, -lat, -long) %>%
@@ -187,7 +370,7 @@ summary(lm(day_exp ~ day_exp_p, data = nearest_50_nab_pheno))
 
 ### Landon's pollen data in Texas ############################################################
 # load in NAB data 
-lb <- readr::read_csv("C:/Users/dsk856/Box/texas/NASA_project/landon_daily_measurements_2009_2011.csv") %>%
+lb <- readr::read_csv("C:/Users/danka/Box/texas/NASA_project/landon_daily_measurements_2009_2011.csv") %>%
   mutate(date = ymd(date),
          mo = month(date),
          year = year(date),
@@ -258,7 +441,7 @@ ggplot(lb, aes(x = day_exp, y = cup_m_cumu_dist + 1)) + geom_line() + theme_bw()
 
 
 ### cone opening preds compared to landon's data #########################################################################
-pheno_preds_lb <- read_csv("C:/Users/dsk856/Box/texas/pheno/manual_obs_models/NAB_opening_preds_jan_dec_prcp_25km.csv") %>%
+pheno_preds_lb <- read_csv("C:/Users/danka/Box/texas/pheno/manual_obs_models/NAB_opening_preds_jan_dec_prcp_25km.csv") %>%
   rename(day_exp = day_experiment,
          yr_exp = fs_year,
          site = NAB_station) %>%
